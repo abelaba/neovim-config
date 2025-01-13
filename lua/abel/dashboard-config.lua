778f442217b6c078255258b7a37a5ee9ff0ee189
abelaba <se.abel.abate@gmail.com>
2025-01-13 22:35:19 +0300

    Add project and mru property to dashboard configuration

--- lua/abel/dashboard-config.lua
+++ lua/abel/dashboard-config.lua
@@ -13,6 +13,8 @@ return {
 		[[Continuous improvement is the key to success. Start through small, consistent steps.]],
 		[[]]
 	},
+	project = { enable = true, limit = 8 },
+  	mru = { enable = true, limit = 10 },
 	footer = {
 		[[]],
 		[[Every end is a new beginning. Keep moving forward!]],
