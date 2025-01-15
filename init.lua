9db21c5393b4de4f64d85bed87b46fb60133a163
abelaba <se.abel.abate@gmail.com>
2025-01-15 22:01:25 +0300

    Update plugins

--- init.lua
+++ init.lua
@@ -39,166 +39,139 @@ vim.keymap.set('n', '<leader>cc', function()
   print("Codeium virtual text is now " .. (not virtual_text.manual and "enabled" or "disabled"))
 end, { noremap = true, silent = true, desc = "Toggle Codeium virtual text" })
 
+vim.api.nvim_create_autocmd("BufWritePost", {
+    pattern = "*",
+    callback = function()
+        vim.cmd("FormatWrite")
+    end,
+})
+
 -- Setup lazy.nvim
 require("lazy").setup({
 	spec = {
-		-- add your plugins here
+		{
+			"NeogitOrg/neogit",
+			dependencies = {
+				"nvim-lua/plenary.nvim",        
+				"sindrets/diffview.nvim", 
+				"nvim-telescope/telescope.nvim",
+			},
+			config = true
+		},
 		{
 			'nvim-telescope/telescope.nvim', tag = '0.1.8',
 			dependencies = { 'nvim-lua/plenary.nvim' }
 		},
-		{ "rose-pine/neovim", name = "rose-pine" },
 		{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-		-- {
-			--	"NeogitOrg/neogit",
-			--	dependencies = {
-				--		"nvim-lua/plenary.nvim", 
-				--		"sindrets/diffview.nvim",
-				--	},
-				--	config = true
-				-- },
+		{
+			"goolord/alpha-nvim",
+			dependencies = { 'nvim-tree/nvim-web-devicons' },
+			config = function()
+				local startify = require("alpha.themes.startify")
+				startify.file_icons.provider = "devicons"
+				require("alpha").setup(startify.config)
+			end,
+		},
+		{ "lewis6991/gitsigns.nvim", name = "gitsigns" },
+		{
+			'nvim-lualine/lualine.nvim',
+			dependencies = { 'nvim-tree/nvim-web-devicons' },
+		},
+		{
+			"folke/which-key.nvim",
+			event = "VeryLazy",
+			keys = {
 				{
-					"kdheepak/lazygit.nvim",
-					lazy = true,
-					cmd = {
-						"LazyGit",
-						"LazyGitConfig",
-						"LazyGitCurrentFile",
-						"LazyGitFilter",
-						"LazyGitFilterCurrentFile",
-					},
-					-- optional for floating window border decoration
-					dependencies = {
-						"nvim-lua/plenary.nvim",
-					},
-					-- setting the keybinding for LazyGit with 'keys' is recommended in
-					-- order to load the plugin when the command is run for the first time
-					keys = {
-						{ "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
-					},
-					config = function ()
-						require("telescope").load_extension("lazygit")
-					end
+					"<leader>?",
+					function()
+						require("which-key").show({ global = false })
+					end,
+					desc = "Buffer Local Keymaps (which-key)",
 				},
-				{
-					'nvimdev/dashboard-nvim',
-					event = 'VimEnter',
-					opts = {
-						theme = 'hyper',
-						shortcut_type = 'number',
-						config = require 'abel.dashboard-config',
-					},
-					dependencies = { {'nvim-tree/nvim-web-devicons'}}
-				},
-				{ "lewis6991/gitsigns.nvim", name = "gitsigns" },
-				{
-					'nvim-lualine/lualine.nvim',
-					dependencies = { 'nvim-tree/nvim-web-devicons' },
-				},
-				{
-					"folke/which-key.nvim",
-					event = "VeryLazy",
-					keys = {
-						{
-							"<leader>?",
-							function()
-								require("which-key").show({ global = false })
-							end,
-							desc = "Buffer Local Keymaps (which-key)",
-						},
-					},
-				},
-				{
-					"nvim-neo-tree/neo-tree.nvim",
-					branch = "v3.x",
-					dependencies = {
-						"nvim-lua/plenary.nvim",
-						"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-						"MunifTanjim/nui.nvim",
-						"3rd/image.nvim", -- Optional image support in preview window: See # Preview Mode for more information
-					}
-				},
-				{
-					"kawre/leetcode.nvim",
-					build = ":TSUpdate html", -- if you have nvim-treesitter installed
-					dependencies = {
-						"nvim-telescope/telescope.nvim",
-						"nvim-lua/plenary.nvim",
-						"MunifTanjim/nui.nvim",
-					},
-					opts = {
-						arg = "leet",
-						lang = 'python3',
-					}
-				},
-				{
-					"williamboman/mason.nvim",
-					"williamboman/mason-lspconfig.nvim",
-					"neovim/nvim-lspconfig",
-				},
-				{
-					"Mofiqul/vscode.nvim",
-				},
-				{
-					"ThePrimeagen/harpoon",
-					branch = "harpoon2",
-					dependencies = { "nvim-lua/plenary.nvim" }
-				},
-				{
-					"Exafunction/codeium.nvim",
-					dependencies = {
-						"nvim-lua/plenary.nvim",
-						"hrsh7th/nvim-cmp",
-					},
-				},
-				{
-					'hrsh7th/nvim-cmp',
-					dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
-				},
-				{
-					"folke/noice.nvim",
-					event = "VeryLazy",
-					dependencies = {
-						"MunifTanjim/nui.nvim",
-						"rcarriga/nvim-notify",
-					}
-				},
-				{ 'mhartington/formatter.nvim' },
-				{
-					'nvim-flutter/flutter-tools.nvim',
-					lazy = false,
-					dependencies = {
-						'nvim-lua/plenary.nvim',
-						'stevearc/dressing.nvim', -- optional for vim.ui.select
-					},
-					config = true,
-				},
-				{'akinsho/toggleterm.nvim', version = "*", opts = { direction = 'float', size = 20 }},
-				{'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'},
-				{
-					"karb94/neoscroll.nvim",
-					opts = {},
-				},
-				--{
-					--	"m4xshen/hardtime.nvim",
-					--	dependencies = { "MunifTanjim/nui.nvim" },
-					--	opts = {}
-					-- },
-					{
-						'tamton-aquib/duck.nvim',
-						config = function()
-							vim.keymap.set('n', '<leader>dd', function() require("duck").hatch("🦍") end, {})
-							vim.keymap.set('n', '<leader>dk', function() require("duck").cook() end, {})
-							vim.keymap.set('n', '<leader>da', function() require("duck").cook_all() end, {})
-						end
-					},
-				},
-				-- Configure any other settings here. See the documentation for more details.
-				-- colorscheme that will be used when installing plugins.
-				install = { colorscheme = { "vscode" } },
-				-- automatically check for plugin updates
-				checker = { enabled = true },
-			})
+			},
+		},
+		{
+			"nvim-neo-tree/neo-tree.nvim",
+			branch = "v3.x",
+			dependencies = {
+				"nvim-lua/plenary.nvim",
+				"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+				"MunifTanjim/nui.nvim",
+				"3rd/image.nvim", -- Optional image support in preview window: See # Preview Mode for more information
+			}
+		},
+		{
+			"kawre/leetcode.nvim",
+			build = ":TSUpdate html", -- if you have nvim-treesitter installed
+			dependencies = {
+				"nvim-telescope/telescope.nvim",
+				"nvim-lua/plenary.nvim",
+				"MunifTanjim/nui.nvim",
+			},
+			opts = {
+				arg = "leet",
+				lang = 'python3',
+			}
+		},
+		{
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"neovim/nvim-lspconfig",
+		},
+		{
+			"Mofiqul/vscode.nvim",
+		},
+		{
+			"ThePrimeagen/harpoon",
+			branch = "harpoon2",
+			dependencies = { "nvim-lua/plenary.nvim" }
+		},
+		{
+			"Exafunction/codeium.nvim",
+			dependencies = {
+				"nvim-lua/plenary.nvim",
+				"hrsh7th/nvim-cmp",
+			},
+		},
+		{
+			'hrsh7th/nvim-cmp',
+			dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
+		},
+		{
+			"folke/noice.nvim",
+			event = "VeryLazy",
+			dependencies = {
+				"MunifTanjim/nui.nvim",
+				"rcarriga/nvim-notify",
+			}
+		},
+		{ 'mhartington/formatter.nvim' },
+		{
+			'nvim-flutter/flutter-tools.nvim',
+			lazy = false,
+			dependencies = {
+				'nvim-lua/plenary.nvim',
+				'stevearc/dressing.nvim', -- optional for vim.ui.select
+			},
+			config = true,
+		},
+		{ 'akinsho/toggleterm.nvim', version = "*", opts = { direction = 'float', size = 20 }},
+		{ 'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons' },
+		{
+			"karb94/neoscroll.nvim",
+			opts = {},
+		},
+		{
+			'numToStr/Comment.nvim',
+			opts = {}
+		},
+	},
+	-- Configure any other settings here. See the documentation for more details.
+	-- colorscheme that will be used when installing plugins.
+	install = { colorscheme = { "vscode" } },
+	-- automatically check for plugin updates
+	checker = { enabled = true },
+})
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
@@ -220,17 +193,9 @@ require("formatter").setup({
 	},
 })
 
-vim.api.nvim_create_autocmd("BufWritePost", {
-    pattern = "*", 
-    callback = function()
-        vim.cmd("FormatWrite")
-    end,
-})
-
 require("notify").setup({
-  background_colour = "#000000",
+	background_colour = "#000000",
 })
-
 require("flutter-tools").setup {}
 require('neoscroll').setup({
 	mappings = {                 -- Keys to be mapped to their corresponding default scrolling animation
@@ -273,3 +238,4 @@ require'lspconfig'.lua_ls.setup{
     },
   },
 }
+
