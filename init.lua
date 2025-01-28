ef5d769d3c22d938f6e980984e31be0e64920f9b
abelaba <se.abel.abate@gmail.com>
2025-01-28 15:10:15 +0300

    Add noice plugin

--- init.lua
+++ init.lua
@@ -49,6 +49,14 @@ vim.g.neovide_fullscreen = true
 require("lazy").setup({
 	spec = {
 		{
+			"folke/noice.nvim",
+			event = "VeryLazy",
+			dependencies = {
+				"MunifTanjim/nui.nvim",
+				-- "rcarriga/nvim-notify",
+			},
+		},
+		{
 			"NeogitOrg/neogit",
 			dependencies = {
 				"nvim-lua/plenary.nvim",
@@ -182,6 +190,25 @@ require("lspconfig").lua_ls.setup({
 				globals = { "vim" }, -- Recognize 'vim' as a global variable
 			},
 		},
+	},
+})
+
+require("noice").setup({
+	lsp = {
+		-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+		override = {
+			["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+			["vim.lsp.util.stylize_markdown"] = true,
+			["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+		},
+	},
+	-- you can enable a preset for easier configuration
+	presets = {
+		bottom_search = true, -- use a classic bottom cmdline for search
+		command_palette = true, -- position the cmdline and popupmenu together
+		long_message_to_split = true, -- long messages will be sent to a split
+		inc_rename = false, -- enables an input dialog for inc-rename.nvim
+		lsp_doc_border = false, -- add a border to hover docs and signature help
 	},
 })
 
