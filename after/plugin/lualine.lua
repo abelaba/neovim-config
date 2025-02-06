57964cedccc9ee170d3f53bfdee05c2e8d6f76fe
abelaba <se.abel.abate@gmail.com>
2025-02-06 23:57:56 +0300

    Update lualine colors

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,62 +1,62 @@
--- Bubbles config for lualine
--- Author: lokesh-krishna
+-- Bubbles config for lualine with enhanced visuals
+-- Author: lokesh-krishna (modified with additional enhancements)
 -- MIT license, see LICENSE for more details.
 
--- stylua: ignore
 local colors = {
-  blue   = '#80a0ff',
-  cyan   = '#79dac8',
-  black  = '#080808',
-  white  = '#c6c6c6',
-  red    = '#ff5189',
-  violet = '#d183e8',
-  grey   = '#303030',
+	magenta = "#ff66b2",
+	blue = "#66d9ff",
+	cyan = "#66ffcc",
+	red = "#ff6666",
+	yellow = "#ffeb66",
+	black = "#202020",
+	grey = "#505050",
+	white = "#ffffff",
 }
 
-local bubbles_theme = {
-  normal = {
-    a = { fg = colors.black, bg = colors.violet },
-    b = { fg = colors.white, bg = colors.grey },
-    c = { fg = colors.white },
-  },
-
-  insert = { a = { fg = colors.black, bg = colors.blue } },
-  visual = { a = { fg = colors.black, bg = colors.cyan } },
-  replace = { a = { fg = colors.black, bg = colors.red } },
-
-  inactive = {
-    a = { fg = colors.white, bg = colors.black },
-    b = { fg = colors.white, bg = colors.black },
-    c = { fg = colors.white },
-  },
+local neon_theme = {
+	normal = {
+		a = { fg = colors.black, bg = colors.white, gui = "bold" },
+		b = { fg = colors.white, bg = colors.black },
+		c = { fg = colors.white },
+	},
+	insert = { a = { fg = colors.black, bg = colors.blue, gui = "bold" } },
+	visual = { a = { fg = colors.black, bg = colors.cyan, gui = "bold" } },
+	replace = { a = { fg = colors.black, bg = colors.red, gui = "bold" } },
+	command = { a = { fg = colors.black, bg = colors.yellow, gui = "bold" } },
+	inactive = {
+		a = { fg = colors.white, bg = colors.black },
+		b = { fg = colors.white, bg = colors.black },
+		c = { fg = colors.white },
+	},
 }
 
-require('lualine').setup {
-  options = {
-    theme = bubbles_theme,
-    component_separators = '',
-    section_separators = { left = '', right = '' },
-  },
-  sections = {
-    lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
-    lualine_b = { 'filename', 'branch' },
-    lualine_c = {
-      '%=', --[[ add your center compoentnts here in place of this comment ]]
-    },
-    lualine_x = {},
-    lualine_y = { 'filetype', 'progress' },
-    lualine_z = {
-      { 'location', separator = { right = '' }, left_padding = 2 },
-    },
-  },
-  inactive_sections = {
-    lualine_a = { 'filename' },
-    lualine_b = {},
-    lualine_c = {},
-    lualine_x = {},
-    lualine_y = {},
-    lualine_z = { 'location' },
-  },
-  tabline = {},
-  extensions = {},
-}
+require("lualine").setup({
+	options = {
+		theme = neon_theme,
+		component_separators = { left = "|", right = "|" },
+		section_separators = { left = "", right = "" },
+		icons_enabled = true,
+	},
+	sections = {
+		lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
+		lualine_b = {
+			{ "branch", icon = "" },
+		},
+		lualine_c = {
+			{ "filename", path = 1, symbols = { modified = " ●", readonly = " ", unnamed = "[No Name]" } },
+		},
+		lualine_x = { "encoding", "filetype" },
+		lualine_y = { "progress" },
+		lualine_z = { { "location", separator = { right = "" } } },
+	},
+	inactive_sections = {
+		lualine_a = { { "filename", path = 1 } },
+		lualine_b = {},
+		lualine_c = {},
+		lualine_x = {},
+		lualine_y = {},
+		lualine_z = { "location" },
+	},
+	tabline = {},
+	extensions = {},
+})
