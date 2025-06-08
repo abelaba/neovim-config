89ee0fd0c4643b1ca1d6884cbc881edf76becb54
abelaba <se.abel.abate@gmail.com>
2025-06-08 03:08:53 +0300

    Update mason lsp config import

--- lua/abel/plugins/mason.lua
+++ lua/abel/plugins/mason.lua
@@ -1,15 +1,16 @@
 return {
-	{ "williamboman/mason.nvim", config = true },
+	{ "williamboman/mason.nvim", opts = {} },
 	{
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
-			handlers = {
-				function(server_name)
-					local capabilities = require("blink.cmp").get_lsp_capabilities()
-					require("lspconfig")[server_name].setup({ capabilities = capabilities })
-				end,
-			}
+			require("mason-lspconfig").setup({
+				handlers = {
+					function(server_name)
+						local capabilities = require("blink.cmp").get_lsp_capabilities()
+						require("lspconfig")[server_name].setup({ capabilities = capabilities })
+					end,
+				},
+			})
 		end,
 	},
 }
-
