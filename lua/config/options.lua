98f3d9aabb83fc4b1238b69ea243ac96d2c6308a
abelaba <se.abel.abate@gmail.com>
2025-06-14 00:19:27 +0300

    Set default terminal to pwsh

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
---
+
+vim.opt.shell = "pwsh"
+vim.o.shellcmdflag = "-NoLogo -NoProfile -Command" -- Make sure to setup mapleader and maplocalleader before
+vim.o.shellquote = [["]]
+vim.o.shellxquote = ""
 require("config.neovide")
