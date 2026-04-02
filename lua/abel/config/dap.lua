89e2bd79c693790801b554d2b179e8e8aec447d4
Abel Abate <se.abel.abate@gmail.com>
2026-04-03 00:06:11 +0200

    update dap config and add spell check for markdown files

--- lua/abel/config/dap.lua
+++ lua/abel/config/dap.lua
@@ -41,68 +41,16 @@ dap.listeners.after.event_initialized["dapui_config"] = function()
 	dapui.open()
 end
 
-local C = require("catppuccin.palettes").get_palette()
 
-local debug_theme = {
-	normal = {
-		a = { bg = C.peach, fg = C.base, gui = "bold" },
-		b = { bg = C.surface1, fg = C.text },
-		c = { bg = C.base, fg = C.text },
-	},
+require("dap-python").setup(vim.env.VIRTUAL_ENV)
 
-	insert = {
-		a = { bg = C.green, fg = C.base, gui = "bold" },
-		b = { bg = C.surface1, fg = C.text },
-		c = { bg = C.base, fg = C.text },
-	},
-
-	visual = {
-		a = { bg = C.blue, fg = C.base, gui = "bold" },
-		b = { bg = C.surface1, fg = C.text },
-		c = { bg = C.base, fg = C.text },
-	},
-
-	replace = {
-		a = { bg = C.mauve, fg = C.base, gui = "bold" },
-		b = { bg = C.surface1, fg = C.text },
-		c = { bg = C.base, fg = C.text },
-	},
-
-	command = {
-		a = { bg = C.yellow, fg = C.base, gui = "bold" },
-		b = { bg = C.surface1, fg = C.text },
-		c = { bg = C.base, fg = C.text },
-	},
-
-	inactive = {
-		a = { bg = C.mantle, fg = C.overlay1 },
-		b = { bg = C.mantle, fg = C.overlay1 },
-		c = { bg = C.mantle, fg = C.overlay1 },
-	},
-}
-
-local lualine = require("lualine")
-local function set_debug_lualine_theme()
-	lualine.setup({ options = { theme = debug_theme } })
-end
-
-local function restore_lualine_theme()
-	lualine.setup({ options = { theme = "auto" } })
-end
-
-dap.listeners.after.event_initialized["lualine_theme"] = set_debug_lualine_theme
-dap.listeners.before.event_terminated["lualine_theme"] = restore_lualine_theme
-dap.listeners.before.event_exited["lualine_theme"] = restore_lualine_theme
-
-
-require("dap-python").setup(vim.fn.getcwd() .. "/.venv/bin/python")
 dap.configurations.python = {
 	{
 		type = "python",
 		request = "launch",
 		name = "Click: run slidev",
 		program = "run_script.py",
-		justMyCode = true,
+		justMyCode = false,
 		console = "integratedTerminal",
 	},
 }
--- lua/abel/plugins/snacks.lua
+++ lua/abel/plugins/snacks.lua
@@ -532,12 +532,12 @@ return {
 					Snacks.toggle.diagnostics():map("<leader>ud")
 					Snacks.toggle.line_number():map("<leader>ul")
 					Snacks.toggle
-						.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
-						:map("<leader>uc")
+							.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
+							:map("<leader>uc")
 					Snacks.toggle.treesitter():map("<leader>uT")
 					Snacks.toggle
-						.option("background", { off = "light", on = "dark", name = "Dark Background" })
-						:map("<leader>ub")
+							.option("background", { off = "light", on = "dark", name = "Dark Background" })
+							:map("<leader>ub")
 					Snacks.toggle.inlay_hints():map("<leader>uh")
 					Snacks.toggle.indent():map("<leader>ug")
 					Snacks.toggle.dim():map("<leader>uD")
@@ -577,7 +577,7 @@ return {
 							desc = "Config",
 							action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
 						},
-						{ icon = " ", key = "p", desc = "Projects", action = "<leader>fp" },
+						{ icon = " ", key = "p", desc = "Projects", action = "<leader>qS" },
 						{ icon = " ", key = "s", desc = "Restore Session", section = "session" },
 						{ icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
 						{ icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
